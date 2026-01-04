f877d2af6070562b562463c26ef49d5e94fa8adb
xlebusek-kate <iriskadeniska195@gmail.com>
2026-01-04 23:24:24 +0300

    ДЗ 11

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,51 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-
+    // Задание номер 1
+    public static void checkYears(int year) {
+        if (year <= 1584) {
+            System.out.println(year + " год — невисокосный год.");
+        } else if (year % 400 == 0) {
+            System.out.println(year + " год — високосный год.");
+        } else if (year % 100 == 0) {
+            System.out.println(year + " год — невисокосный год.");
+        } else if (year % 4 == 0) {
+            System.out.println(year + " год — високосный год.");
+        } else System.out.println(year + " год — невисокосный год.");
     }
+
+    // Задание номер 2
+    public static void checkVersions(int number, int clientDeviceYear) {
+        if (number == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите обычную версию приложения для iOS по ссылке");
+        } else if (number == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (number == 1 && clientDeviceYear >= 2015) {
+            System.out.println("Установите обычную версию приложения для Android по ссылке");
+        } else if (number == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }else {
+            System.out.println("Ошибка");
+        }
+    }
+
+    // Задание номер 3
+    public static void checkDistance(int deliveryDistance ){
+        if (deliveryDistance >= 60 && deliveryDistance <= 100) {
+            System.out.println("Потребуется дней: 3");
+        } else if (deliveryDistance > 20 && deliveryDistance < 60) {
+            System.out.println("Потребуется дней: 2");
+        } else if (deliveryDistance <= 20 && deliveryDistance > 0) {
+            System.out.println("Потребуется дней: 1");
+        } else {
+            System.out.println("Доставки нет");
+        }
+    }
+
+    public static void main(String[] args) {
+        checkYears(2000);
+        checkVersions(1, 2026);
+        int deliveryDistance = 95;
+        checkDistance(deliveryDistance);
+    }
+
+
 }
