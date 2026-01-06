e263359caa65b3f86a53daf0e0c12291c0b6c648
xlebusek-kate <iriskadeniska195@gmail.com>
2026-01-06 19:02:33 +0300

    ДЗ 11

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.time.LocalDate;
+
 public class Main {
     // Задание номер 1
     public static void checkYears(int year) {
@@ -16,13 +18,14 @@ public class Main {
 
     // Задание номер 2
     public static void checkVersions(int number, int clientDeviceYear) {
-        if (number == 0 && clientDeviceYear >= 2015) {
+        int currentYear = LocalDate.now().getYear();
+        if (number == 0 && clientDeviceYear == currentYear) {
             System.out.println("Установите обычную версию приложения для iOS по ссылке");
-        } else if (number == 0 && clientDeviceYear < 2015) {
+        } else if (number == 0 && clientDeviceYear < currentYear) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        } else if (number == 1 && clientDeviceYear >= 2015) {
+        } else if (number == 1 && clientDeviceYear == currentYear) {
             System.out.println("Установите обычную версию приложения для Android по ссылке");
-        } else if (number == 1 && clientDeviceYear < 2015) {
+        } else if (number == 1 && clientDeviceYear < currentYear) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
         } else {
             System.out.println("Ошибка");
@@ -30,15 +33,19 @@ public class Main {
     }
 
     // Задание номер 3
-    public static void checkDistance(int deliveryDistance) {
-        if (deliveryDistance >= 60 && deliveryDistance <= 100) {
-            System.out.println("Потребуется дней: 3");
+    public static int checkDistance(int deliveryDistance) {
+        byte days = 0;
+        if (deliveryDistance <= 20 && deliveryDistance > 0) {
+            days = 1;
+            return days;
         } else if (deliveryDistance > 20 && deliveryDistance < 60) {
-            System.out.println("Потребуется дней: 2");
-        } else if (deliveryDistance <= 20 && deliveryDistance > 0) {
-            System.out.println("Потребуется дней: 1");
+            days = 2;
+            return days;
+        } else if (deliveryDistance >= 60 && deliveryDistance <= 100) {
+            days = 3;
+            return days;
         } else {
-            System.out.println("Доставки нет");
+            return days;
         }
     }
 
@@ -47,7 +54,7 @@ public class Main {
         checkVersions(1, 2026);
         int deliveryDistance = 95;
         checkDistance(deliveryDistance);
+        System.out.println("Потребуется дней: "+ checkDistance(deliveryDistance));
     }
-
-
 }
+
